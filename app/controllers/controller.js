24338d54a6fdb73c3a47e86fc94afb68e7400a73
alibombaye <alibombaye@gmail.com>
2019-08-08 16:18:41 +0100

    extract solution-card-sections to a seperate macro file

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,10 +1,26 @@
 import { createShowCardPageContext } from './../contextCreators/showCardsPageContext';
 import axios from 'axios';
 
+const config = {
+  "description": {
+    showTitle: false,
+    columns: 1
+  },
+  "features": {
+    showTitle: true,
+    columns: 1
+  },
+  "capability-section": {
+    showTitle: true,
+    columns: 2
+  }
+}
+
+
 export const getShowCardsPageContext = async () => {
   const solutionData = await axios.get('http://localhost:5000/api/v1/solutions');
 
-  const context = createShowCardPageContext(solutionData.data.solutions);
+  const context = createShowCardPageContext(solutionData.data.solutions, config);
 
   return context;
 }
