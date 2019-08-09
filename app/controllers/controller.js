9f9532cc8e7909a4d32beb1f5e717f0dfb3ae01d
alibombaye <alibombaye@gmail.com>
2019-08-09 12:57:43 +0100

    pass config to context creator

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -20,7 +20,7 @@ const config = {
 export const getShowCardsPageContext = async () => {
   const solutionData = await axios.get('http://localhost:5000/api/v1/solutions');
 
-  const context = createShowCardPageContext(solutionData.data.solutions);
+  const context = createShowCardPageContext(solutionData.data.solutions, config);
 
   return context;
 }
