06794f61aaabb8b396e8f8980b2ba12d154dc09d
alibombaye <alibombaye@gmail.com>
2019-08-14 10:53:30 +0100

    removed hard coded context

--- app/routes.js
+++ app/routes.js
@@ -4,79 +4,9 @@ import { getShowCardsPageContext, getSolutionPageContext } from './controller';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  // const context = {
-  //   solutions: [
-  //     {
-  //       id: "00001",
-  //       name: "The first solution",
-  //       sections: [
-  //         {
-  //           name: "Description",
-  //           value: "First the worse",
-  //         },
-  //         {
-  //           name: "Features",
-  //           value: [
-  //             "feature 1",
-  //             "feature 2",
-  //             "feature 3",
-  //           ],
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       id: "00002",
-  //       name: "The second solution",
-  //       sections: [
-  //         {
-  //           name: "Description",
-  //           value: "Second the best",
-  //         },
-  //         {
-  //           name: "Features",
-  //           value: [
-  //             "feature 4",
-  //             "feature 5",
-  //             "feature 6",
-  //           ],
-  //         },
-  //         {
-  //           name: "SAy WhaTTT!!! lots",
-  //           value: [
-  //             "WHatttt!!!",
-  //             "WHatttt!!! whattt!!",
-  //           ]
-  //         }
-  //       ],
-  //     },
-  //     {
-  //       id: "00003",
-  //       name: "The third solution",
-  //       sections: [
-  //         {
-  //           name: "Description",
-  //           value: "Third the one with the hairy chest",
-  //         },
-  //         {
-  //           name: "Features",
-  //           value: [
-  //             "feature 7",
-  //             "feature 8",
-  //             "feature 9",
-  //           ],
-  //         },
-  //         {
-  //           name: "SAy WhaTTT!!! once",
-  //           value: "WHatttt!!!"
-  //         }
-  //       ],
-  //     }
-  //   ]
-  // }
+  const context = await getShowCardsPageContext();
 
-  const newContext = await getShowCardsPageContext();
-
-  res.render('index', newContext);
+  res.render('index', context);
 });
 
 router.get('/:solutionId', async (req, res) => {
