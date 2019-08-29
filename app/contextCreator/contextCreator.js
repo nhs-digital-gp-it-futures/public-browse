e6ea5adcb45e3d65ce0bb47d6d201090709d29c2
alibombaye <alibombaye@gmail.com>
2019-08-29 10:59:39 +0100

    extracted createColumnsForSectionValue to seperate file

--- app/contextCreator/contextCreator.js
+++ app/contextCreator/contextCreator.js
@@ -1,25 +1,7 @@
+import { createColumnsForSectionValue } from './createColumnsForSectionValue';
+
 const defaultSectionConfig = {
   showTitle: true,
-};
-
-export const createColumnsForSectionValue = (value) => {
-  const newValue = {};
-  const column1 = [];
-  const column2 = [];
-
-  value.map((v, idx) => {
-    const id = idx + 1;
-    if (Math.ceil(value.length / 2) >= id) {
-      column1.push(v);
-    } else {
-      column2.push(v);
-    }
-  });
-
-  newValue.column1 = column1;
-  newValue.column2 = column2;
-
-  return newValue;
 };
 
 export const applyDefaultConfig = (sections) => {
--- app/contextCreator/contextCreator.test.js
+++ app/contextCreator/contextCreator.test.js
@@ -3,7 +3,6 @@ import {
   createSolutionPageContext,
   applySectionConfig,
   applyDefaultConfig,
-  createColumnsForSectionValue,
   createPageContentsContext,
   createCapabilityFiltersContext,
 } from './contextCreator';
@@ -410,34 +409,6 @@ describe('applyDefaultConfig', () => {
     const context = applyDefaultConfig(initialContext);
 
     expect(context).toEqual(expectedContext);
-  });
-});
-
-describe('createColumnsForSection', () => {
-  it('should split the section values evenly into 2 columns when they are 2 values', () => {
-    const expectedSectionValue = {
-      column1: ['value 1'],
-      column2: ['value 2'],
-    };
-
-    const initialSectionValue = ['value 1', 'value 2'];
-
-    const sectionValue = createColumnsForSectionValue(initialSectionValue);
-
-    expect(sectionValue).toEqual(expectedSectionValue);
-  });
-
-  it('should split the section values with an extra value in the first column when an odd number of values', () => {
-    const expectedSectionValue = {
-      column1: ['value 1', 'value 2'],
-      column2: ['value 3'],
-    };
-
-    const initialSectionValue = ['value 1', 'value 2', 'value 3'];
-
-    const sectionValue = createColumnsForSectionValue(initialSectionValue);
-
-    expect(sectionValue).toEqual(expectedSectionValue);
   });
 });
 
