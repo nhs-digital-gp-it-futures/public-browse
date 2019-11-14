5dca3fdabdbe1fa864069b949f2f40753c63eeb5
kat1906 <katie.armstrong@bjss.com>
2019-11-14 15:35:53 +0000

    pass in label for data-test-id tag

--- app/views/sections/view-solution-description.test.js
+++ app/views/sections/view-solution-description.test.js
@@ -75,7 +75,7 @@ describe('view-solution-description', () => {
           const summaryQuestion = $('[data-test-id="view-section-question-summary"]');
 
           expect(summaryQuestion.find('[data-test-id="view-question-title"]').text().trim()).toEqual('Summary');
-          expect(summaryQuestion.find('[data-test-id="view-question-data-text"]').text().trim()).toEqual('Some summary data');
+          expect(summaryQuestion.find('[data-test-id="view-question-data-text-summary"]').text().trim()).toEqual('Some summary data');
 
           done();
         });
@@ -99,7 +99,7 @@ describe('view-solution-description', () => {
           const descriptionQuestion = $('[data-test-id="view-section-question-description"]');
 
           expect(descriptionQuestion.find('[data-test-id="view-question-title"]').text().trim()).toEqual('About the solution');
-          expect(descriptionQuestion.find('[data-test-id="view-question-data-text"]').text().trim()).toEqual('Some description data');
+          expect(descriptionQuestion.find('[data-test-id="view-question-data-text-description"]').text().trim()).toEqual('Some description data');
 
           done();
         });
