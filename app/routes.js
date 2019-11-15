8d98bbd727556ad789341355bd0db1e904b416de
kat1906 <katie.armstrong@bjss.com>
2019-11-15 13:36:28 +0000

    change from static data to api call

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,7 @@ import {
   getSolutionPageContext,
   postCapabilityFilters,
   getFoundationCapabilitySolutions,
+  getPublicSolutionById,
 } from './controller';
 
 const router = express.Router();
@@ -11,44 +12,10 @@ const router = express.Router();
 router.get('/', async (req, res) => {
   res.render('home-page', {});
 });
+
 router.get('/view-solution/:solutionId', async (req, res) => {
-  const context = {
-    id: 'S100000-001',
-    name: 'Write on Time',
-    organisationName: 'Really Kool Corporation',
-    isFoundation: true,
-    lastUpdated: 'a date',
-    sections: {
-      'solution-description': {
-        answers: {
-          summary: 'Write on Time is a Citizen-facing Appointments Management system specifically designed to reduce the number of DNAs in your practice.',
-          description: 'a description',
-          link: 'link.com',
-        },
-      },
-      'contact-details': {
-        answers: {
-          'contact-1': {
-            'department-name': 'a contact dept',
-            'contact-name': 'jim jones',
-            'phone-number': '0111 111111',
-            'email-address': 'jim@solution.com',
-          },
-          'contact-2': {
-            'department-name': 'a second contact dept',
-            'contact-name': 'jacky johnston',
-            'phone-number': '0222 222222',
-            'email-address': 'jacky@solution.com',
-          },
-        },
-      },
-      capabilities: {
-        answers: {
-          'capabilities-met': ['capability 1', 'capability 2', 'capability 3'],
-        },
-      },
-    },
-  };
+  const { solutionId } = req.params;
+  const context = await getPublicSolutionById(solutionId);
   res.render('view-solution-page', context);
 });
 
