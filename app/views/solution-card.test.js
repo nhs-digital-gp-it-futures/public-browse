c135813ca90baa1ff382f7d7c3740a4ca49b69fd
kat1906 <katie.armstrong@bjss.com>
2019-11-18 16:48:21 +0000

    add view solution link and resize description text

--- app/views/solution-card.test.js
+++ app/views/solution-card.test.js
@@ -45,6 +45,27 @@ describe('solution-card', () => {
       });
   });
 
+  it('should render the view the solution link', (done) => {
+    const context = {
+      solution: {
+        id: 'S1',
+      },
+    };
+
+    const app = createDummyApp(context);
+    request(app)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+        const viewSolutionLink = $('[data-test-id="solution-card-view-link"]');
+
+        expect(viewSolutionLink.length).toEqual(1);
+        expect(viewSolutionLink.text().trim()).toEqual('View the solution');
+        expect(viewSolutionLink.find('a').attr('href')).toEqual(`/view-solution/${context.solution.id}`);
+        done();
+      });
+  });
+
   it('should render the organisation name', (done) => {
     const context = {
       solution: {
@@ -112,7 +133,30 @@ describe('solution-card', () => {
         done();
       });
   });
+  {/* <div data-test-id="solution-card-name" class="nhsuk-u-margin-top-5">
+<a href="/view-solution/{{ solution.id }}">View the solution</a>
+</div> */}
+  it('should render the View the solution link', (done) => {
+    const context = {
+      solution: {
+        organisationName: 'some organisation name',
+      },
+    };
 
+    const app = createDummyApp(context);
+    request(app)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+
+        const solutionOrganisationName = $('[data-test-id="solution-card-organisation"]');
+
+        expect(solutionOrganisationName.length).toEqual(1);
+        expect(solutionOrganisationName.text().trim()).toEqual('some organisation name');
+
+        done();
+      });
+  });
   describe('capability list', () => {
     it('should render 0 capability names if no capabilities are provided in the context', (done) => {
       const context = {
