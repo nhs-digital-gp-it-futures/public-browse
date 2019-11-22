ee02167eb99de7cec08f2b9fca3a4d345640a28b
kat1906 <katie.armstrong@bjss.com>
2019-11-22 09:32:15 +0000

    merge master

--- app/solutions-list/controller.js
+++ app/solutions-list/controller.js
@@ -1,22 +1,18 @@
 import axios from 'axios';
 import { createSolutionListPageContext, createFoundationSolutionListPageContext } from './context';
 
-export const getSolutionFoundationListPageContext = async () => {
-  const foundationSolutionListResponse = await axios.get('http://localhost:8080/api/v1/Solutions/Foundation');
-
-  const context = createFoundationSolutionListPageContext(
-    foundationSolutionListResponse.data.solutions,
-  );
-
-  return context;
-};
-
-export const getSolutionListPageContext = async () => {
-  const solutionListResponse = await axios.get('http://localhost:8080/api/v1/Solutions');
-
-  const context = createSolutionListPageContext(
-    solutionListResponse.data.solutions,
-  );
-
-  return context;
+export const getSolutionListPageContext = async (filterType) => {
+  if (filterType === 'all') {
+    const solutionListResponse = await axios.get('http://localhost:8080/api/v1/Solutions');
+    return createSolutionListPageContext(
+      solutionListResponse.data.solutions,
+    );
+  }
+  if (filterType === 'foundation') {
+    const foundationSolutionListResponse = await axios.get('http://localhost:8080/api/v1/Solutions/Foundation');
+    return createFoundationSolutionListPageContext(
+      foundationSolutionListResponse.data.solutions,
+    );
+  }
+  return undefined;
 };
