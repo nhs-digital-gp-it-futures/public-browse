0c2e9a65a208d47b2e83ee73c17868b2a68c5d26
kat1906 <katie.armstrong@bjss.com>
2019-11-26 13:22:25 +0000

    extract footerLinks into manifest

--- app/includes/footer.test.js
+++ app/includes/footer.test.js
@@ -2,51 +2,40 @@ import request from 'supertest';
 import express from 'express';
 import cheerio from 'cheerio';
 import { App } from '../../app';
+import content from '../manifest.json';
 
 const createDummyApp = (context) => {
   const app = new App().createApp();
-
   const router = express.Router();
   const dummyRouter = router.get('/', (req, res) => {
     res.render('includes/footer.njk', context);
   });
-
   app.use(dummyRouter);
-
   return app;
 };
 
 
 describe('footer', () => {
   it('should render the footer panel', (done) => {
-    const context = {};
-
+    const context = content;
     const app = createDummyApp(context);
     request(app)
       .get('/')
       .then((res) => {
         const $ = cheerio.load(res.text);
         const footer = $('[data-test-id="footer"]');
-
         expect(footer.length).toEqual(1);
-
         const footerComponent = footer.find('[data-test-id="footer-component"]');
-
         expect(footerComponent.length).toEqual(1);
-
-        expect(footerComponent.find('li:nth-child(1)').text().trim()).toEqual("Buyer's Guide");
-        expect(footerComponent.find('li:nth-child(2)').text().trim()).toEqual('NHS Digital service desk');
-        expect(footerComponent.find('li:nth-child(3)').text().trim()).toEqual('NHS Digital');
-        expect(footerComponent.find('li:nth-child(4)').text().trim()).toEqual('About GPIT Futures');
-        expect(footerComponent.find('li:nth-child(5)').text().trim()).toEqual('Capabilities and Standards Model');
-
+        content.footerLinks.map((link, i) => {
+          expect(footerComponent.find(`li:nth-child(${i + 1})`).text().trim()).toEqual(link.label);
+        });
         done();
       });
   });
 
   it('should render the footer legal panel', (done) => {
-    const context = {};
-
+    const context = content;
     const app = createDummyApp(context);
     request(app)
       .get('/')
@@ -54,7 +43,6 @@ describe('footer', () => {
         const $ = cheerio.load(res.text);
         const footer = $('[data-test-id="footer"]');
         const legalPanel = footer.find('[data-test-id="legal-panel"]');
-
         expect(footer.length).toEqual(1);
         expect(legalPanel.length).toEqual(1);
         expect(legalPanel.find('span:nth-child(1)').text().trim()).toEqual('Legal');
--- app/routes.js
+++ app/routes.js
@@ -3,28 +3,29 @@ import { getPublicSolutionById } from './view-solution/controller';
 import { getSolutionListPageContext } from './solutions-list/controller';
 import { getBrowseSolutionsPageContext } from './browse-solutions/context';
 import { getHomepageContext } from './homepage/context';
+import footerLinks from './manifest.json';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const context = getHomepageContext();
+  const context = { ...getHomepageContext(), ...footerLinks };
   res.render('homepage/template.njk', context);
 });
 
 router.get('/solutions', (req, res) => {
-  const context = getBrowseSolutionsPageContext();
+  const context = { ...getBrowseSolutionsPageContext(), ...footerLinks };
   res.render('browse-solutions/template.njk', context);
 });
 
 router.get('/solutions/:filterType', async (req, res) => {
   const { filterType } = req.params;
-  const context = await getSolutionListPageContext(filterType);
+  const context = { ...await getSolutionListPageContext(filterType), ...footerLinks };
   res.render('solutions-list/template.njk', context);
 });
 
 router.get('/solutions/:filterType/:solutionId', async (req, res) => {
   const { solutionId } = req.params;
-  const context = await getPublicSolutionById(solutionId);
+  const context = { ...await getPublicSolutionById(solutionId), ...footerLinks };
   res.render('view-solution/template.njk', context);
 });
 
