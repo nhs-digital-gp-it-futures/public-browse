8ea4a67f9fb75438406cc035584f4192bb06c052
kat1906 <katie.armstrong@bjss.com>
2019-11-28 15:15:55 +0000

    made a start on ui test

---
+++ app/guide/ui.test.js
@@ -0,0 +1,34 @@
+import { Selector } from 'testcafe';
+import content from './manifest.json';
+
+const pageSetup = async (t) => {
+  await t.navigateTo('http://localhost:1234/guide');
+};
+
+fixture('Show Buyers Guide Page');
+
+test('should render the title', async (t) => {
+  pageSetup(t);
+  const title = Selector('[data-test-id="guide-page-title"]');
+  await t
+    .expect(title.count).eql(1)
+    .expect(title.innerText).eql(content.title);
+});
+
+test('should render the description', async (t) => {
+  pageSetup(t);
+  const description = Selector('[data-test-id="guide-page-description"]');
+  await t
+    .expect(description.count).eql(1)
+    .expect(description.innerText).eql(content.description);
+});
+
+test('should render the subtext', async (t) => {
+  pageSetup(t);
+  const subText = Selector('[data-test-id="guide-page-subtext"] > div');
+  await t
+    .expect(subText.find('div').nth(0).innerText).eql(content.subtext[0])
+    .expect(subText.find('div').nth(1).innerText).eql(content.subtext[1])
+    .expect(subText.find('div').nth(2).innerText).eql(content.subtext[2])
+    .expect(subText.find('div').nth(3).innerText).eql(content.subtext[3]);
+});
