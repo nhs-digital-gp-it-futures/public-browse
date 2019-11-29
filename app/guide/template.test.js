cde17273c58a2281e27cf036af07bb8ff71ad6ab
kat1906 <katie.armstrong@bjss.com>
2019-11-29 09:02:26 +0000

    add link to description

--- app/guide/template.test.js
+++ app/guide/template.test.js
@@ -2,6 +2,7 @@ import request from 'supertest';
 import cheerio from 'cheerio';
 import { testHarness } from '../test-utils/testHarness';
 import content from './manifest.json';
+import { start } from 'repl';
 
 const template = 'guide/template.njk';
 
@@ -53,12 +54,42 @@ describe('guide', () => {
         const description = $('[data-test-id="guide-section-description"]');
         const subsection = $('[data-test-id="guide-section-subsection"]');
         expect(title.length).toEqual(content.sections.length);
-        expect(description.length).toEqual(content.sections.length);
         expect(subsection.length).toEqual(content.sections.length);
         content.sections.map((section, i) => {
           expect(title[i].children[0].data).toEqual(section.title);
-          expect(description[i].children[0].data).toEqual(section.description);
+          if (!section.description.href) {
+            expect(description[i].children[0].data.trim()).toEqual(section.description);
+          }
         });
+        done();
+      });
+  });
+
+  it('should render a a tag when description contains href', (done) => {
+    const context = {
+      sections: [{
+        title: 'Catalogue Solution4',
+        description: {
+          startText: 'If you experience any technical issues when using this website, please contact NHS Digital’s Exeter Helpdesk at',
+          linkText: 'exeter.helpdesk@nhs.net',
+          href: 'mailto:exeter.helpdesk@nhs.net',
+          endText: 'or call 0300 303 4034.',
+        },
+      }],
+    };
+    const app = testHarness().createComponentDummyApp(template, context);
+    request(app)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+        const subsectionWithLink = $('[data-test-id="guide-section-description-with-link"]');
+        const startText = subsectionWithLink.find('span:nth-child(1)');
+        const endText = subsectionWithLink.find('span:nth-child(3)');
+        const link = subsectionWithLink.find('a');
+        expect(startText.text().trim()).toEqual(context.sections[0].description.startText.trim());
+        expect(endText.text().trim()).toEqual(context.sections[0].description.endText.trim());
+        expect(link.text().trim()).toEqual(context.sections[0].description.linkText.trim());
+        expect(link.attr('href')).toEqual(context.sections[0].description.href.trim());
         done();
       });
   });
