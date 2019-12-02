fa3e20f44c717e9197c5297ff511dfbec9d51282
Vladimir Pavlicenko <vladimirs.pavlicenko@bjss.com>
2019-12-02 12:11:43 +0000

    wip ui tests

--- app/guide/ui.test.js
+++ app/guide/ui.test.js
@@ -32,3 +32,31 @@ test('should render the subtext', async (t) => {
     .expect(subText.find('div').nth(2).innerText).eql(content.subtext[2])
     .expect(subText.find('div').nth(3).innerText).eql(content.subtext[3]);
 });
+
+test('should catalogue solution header', async (t) => {
+  pageSetup(t);
+  const subText = Selector('[data-test-id="guide-section-title"]');
+  await t
+    .expect(subText.nth(0).innerText).eql(content.sections[0].title)
+});
+
+test('should catalogue solution description', async (t) => {
+  pageSetup(t);
+  const subText = Selector('[data-test-id="guide-section-description"]');
+  await t
+    .expect(subText.nth(0).innerText).eql(content.sections[0].description)
+});
+
+test('should catalogue solution description', async (t) => {
+  pageSetup(t);
+  const subText = Selector('[data-test-id="guide-section-description"]');
+  await t
+    .expect(subText.nth(0).innerText).eql(content.sections[0].description)
+});
+
+test('should catalogue solution subsection 1 title', async (t) => {
+  pageSetup(t);
+  const subText = Selector('[data-test-id="subsection-title"]');
+  await t
+    .expect(subText.nth(0).innerText).eql(content.sections[0].contents[0].title)
+});
--- testcafeRunner.js
+++ testcafeRunner.js
@@ -15,7 +15,7 @@ createTestcafe('localhost')
     server = app.listen('1234');
 
     return tc.createRunner()
-      .src(['**/*ui.test.js'])
+      .src(['**/guide/*ui.test.js'])
       .browsers('chrome:headless')
       .concurrency(2)
       .reporter(['spec', {
