0f2db953972ab8bc2f553ace23be27c3f5aeeeae
kat1906 <katie.armstrong@bjss.com>
2020-02-13 08:57:05 +0000

    pass params in object to getContxt functions

--- app/pages/view-solution/controller.js
+++ app/pages/view-solution/controller.js
@@ -2,8 +2,8 @@ import { createViewSolutionPageContext } from './solutionPageContext';
 import { ApiProvider } from '../../apiProvider';
 import logger from '../../logger';
 
-export const getPublicSolutionById = async (solutionId) => {
-  const response = await new ApiProvider().getPublicSolutionById(solutionId);
+export const getPublicSolutionById = async ({ solutionId }) => {
+  const response = await new ApiProvider().getPublicSolutionById({ solutionId });
 
   if (response.data) {
     logger.info(`Solution ${solutionId}: ${response.data.name} returned`);
