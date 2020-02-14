5a9a531e393a4a14d59098ff071908b97e346dfd
kat1906 <katie.armstrong@bjss.com>
2020-02-14 11:58:56 +0000

    add integration tests

--- app/pages/capability-selector/ui.test.js
+++ app/pages/capability-selector/ui.test.js
@@ -56,9 +56,24 @@ test('should render capability-selector component', async (t) => {
   const column1 = capabilitySelectorComponent.find('[data-test-id="capability-checkbox-column-1"]');
   const column2 = capabilitySelectorComponent.find('[data-test-id="capability-checkbox-column-2"]');
 
-  await t.debug()
+  await t
     .expect(capabilitySelectorComponent.exists).ok()
     .expect(column1.exists).ok()
-    .expect(column1.find('label').length).eql(2)
-    .expect(column2.exists).ok();
+    .expect(column1.find('label').count).eql(2)
+    .expect(await extractInnerText(column1.find('label').nth(0))).eql(capabilitiesData.capabilities[0].name)
+    .expect(await extractInnerText(column1.find('label').nth(1))).eql(capabilitiesData.capabilities[1].name)
+    .expect(column2.find('label').count).eql(1)
+    .expect(await extractInnerText(column2.find('label').nth(0))).eql(capabilitiesData.capabilities[2].name);
+});
+
+test('should render continue button', async (t) => {
+  await pageSetup(t);
+
+  const continueButton = Selector('[data-test-id="capability-selector-continue-button"]');
+
+  await t
+    .expect(continueButton.exists).ok()
+    .expect(await extractInnerText(continueButton.find('a'))).eql('Continue')
+    .expect(continueButton.find('a').hasClass('nhsuk-button--secondary')).ok()
+    .expect(continueButton.find('a').getAttribute('href')).eql('/');
 });
