3f745a6e556c251aaa6551f041e4879314d8a3a8
Fawad Ali <alibombaye@gmail.com>
2020-04-03 10:13:13 +0100

    load solution based upon env

--- app/pages/covid19/controller.js
+++ app/pages/covid19/controller.js
@@ -1,9 +1,16 @@
+import { env } from '../../config';
 import { createCovid19SolutionListPageContext } from './context';
 
 import covid19Manifest from './manifest';
-import covid19Data from './data/covid19-solutions';
+import covid19ProdData from './data/production/covid19-solutions';
+import covid19NonProdData from './data/non-production/covid19-solutions';
 
-export const getCovid19SolutionListPageContext = async () => createCovid19SolutionListPageContext({
-  manifest: covid19Manifest,
-  data: covid19Data,
-});
+
+export const getCovid19SolutionListPageContext = async () => {
+  const covid19Data = env === 'production' ? covid19ProdData : covid19NonProdData;
+
+  return createCovid19SolutionListPageContext({
+    manifest: covid19Manifest,
+    data: covid19Data,
+  });
+};
