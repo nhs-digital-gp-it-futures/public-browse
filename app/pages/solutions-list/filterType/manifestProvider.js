c4c40aaabab0a8cdd78f9c0580665dcd44cd7c18
Ben.Hayward <ben.hayward@bjss.com>
2020-04-28 14:38:14 +0100

    6353: Log manifest error

--- app/pages/solutions-list/filterType/manifestProvider.js
+++ app/pages/solutions-list/filterType/manifestProvider.js
@@ -1,11 +1,13 @@
 import fs from 'fs';
 import path from 'path';
 import { ErrorContext } from 'buying-catalogue-library';
+import { logger } from '../../../logger';
 
 export const getSolutionListManifest = (type) => {
   const filePath = `/${type}/manifest.json`;
 
   if (!fs.existsSync(path.join(__dirname, filePath))) {
+    logger.info(`No manifest found for filter type: ${type}`);
     throw new ErrorContext({
       status: 500,
     });
