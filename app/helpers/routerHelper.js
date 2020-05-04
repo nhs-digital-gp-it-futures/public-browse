a27781fad43e08fbb100b976a0e78e84f05ce303
Fawad Ali <alibombaye@gmail.com>
2020-05-04 11:18:27 +0100

    oops extra blanl line

--- app/helpers/routerHelper.js
+++ app/helpers/routerHelper.js
@@ -1,7 +1,6 @@
 import { ErrorContext } from 'buying-catalogue-library';
 import { getEndpoint } from '../endpoints';
 
-
 export const withCatch = route => async (req, res, next) => {
   try {
     return await route(req, res, next);
