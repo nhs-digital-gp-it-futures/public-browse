a59ca179cbcce4e6407eb38a4aa9af2555860cc7
Keith Lyon <Keith.Lyon@mastek.com>
2021-03-23 16:11:00 +0000

    remove showCovid19 test from header as no longer needed

--- app/includes/header.test.js
+++ app/includes/header.test.js
@@ -94,28 +94,6 @@ describe('header', () => {
       }));
     });
 
-    it('should render the covid19 global warning if feature flag set', componentTester(setup, (harness) => {
-      const context = {
-        config: {
-          showCovid19: 'true',
-        },
-      };
-
-      harness.request(context, ($) => {
-        const globalAlert = $('[data-test-id="covid19-global-alert"]');
-        const title = globalAlert.find('div').find('h2');
-        const paragraph = globalAlert.find('div').find('p');
-        const vaccinationsLink = paragraph.find('[data-test-id="vaccinations"]');
-        const covid19Link = paragraph.find('[data-test-id="covid19"]');
-
-        expect(globalAlert.hasClass('bc-c-global-alert')).toEqual(true);
-        expect(title.text().trim()).toEqual('Coronavirus (COVID-19)');
-        expect(paragraph.text().trim()).toEqual('View Catalogue Solutions that help with coronavirus by organising vaccinations or reducing visits to GP practices.');
-        expect(vaccinationsLink.attr('href')).toEqual('/solutions/vaccinations');
-        expect(covid19Link.attr('href')).toEqual('/solutions/covid19');
-      });
-    }));
-
     it('should not render the covid19 global warning if feature flag is not set', componentTester(setup, (harness) => {
       const context = {
         config: {
