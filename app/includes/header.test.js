a1dbb26f66a3a05da1899b0d92dce2b3f0bba7dd
Keith Lyon <Keith.Lyon@mastek.com>
2021-03-24 09:45:32 +0000

    tweaked covid19 global warning test in header.test

--- app/includes/header.test.js
+++ app/includes/header.test.js
@@ -94,6 +94,26 @@ describe('header', () => {
       }));
     });
 
+    it('should render the covid19 global warning if feature flag set', componentTester(setup, (harness) => {
+      const context = {
+        config: {
+          showCovid19: 'true',
+        },
+      };
+
+      harness.request(context, ($) => {
+        const globalAlert = $('[data-test-id="covid19-global-alert"]');
+        const title = globalAlert.find('div').find('h2');
+        const paragraph = globalAlert.find('div').find('p');
+        const vaccinationsLink = paragraph.find('[data-test-id="vaccinations"]');
+
+        expect(globalAlert.hasClass('bc-c-global-alert')).toEqual(true);
+        expect(title.text().trim()).toEqual('Coronavirus (COVID-19)');
+        expect(paragraph.text().trim()).toEqual('View Catalogue Solutions that help with coronavirus by organising vaccinations.');
+        expect(vaccinationsLink.attr('href')).toEqual('/solutions/vaccinations');
+      });
+    }));
+
     it('should not render the covid19 global warning if feature flag is not set', componentTester(setup, (harness) => {
       const context = {
         config: {
