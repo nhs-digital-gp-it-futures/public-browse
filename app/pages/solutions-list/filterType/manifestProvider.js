9318668c9f80147880a7178f33a2de1275caa37d
bjss-chris <christopher.martin@bjss.com>
2021-05-13 17:39:56 +0100

    Address uncontrolled data in path expression
    AB#14691

--- app/pages/solutions-list/filterType/manifestProvider.js
+++ app/pages/solutions-list/filterType/manifestProvider.js
@@ -1,15 +1,19 @@
 import fs from 'fs';
 import path from 'path';
+import sanitize from 'sanitize-filename';
 import { logger } from '../../../logger';
 
 export const getSolutionListManifest = (type) => {
-  const filePath = `/${type}/manifest.json`;
+  const sanitizedType = sanitize(type);
+  const filePath = `/${sanitizedType}/manifest.json`;
 
   if (!fs.existsSync(path.join(__dirname, filePath))) {
-    logger.error(`No manifest found for filter type: ${type}`);
+    logger.error(`No manifest found for filter type: ${sanitizedType}`);
     throw new Error();
   }
+
   const manifestFileContent = fs.readFileSync(path.join(__dirname, filePath));
   const manifest = JSON.parse(manifestFileContent);
+
   return manifest;
 };
